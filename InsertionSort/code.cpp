d589f542af3ce54e7ecd801fcfcbf6c95031a396
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2021-04-08 08:48:08 +0530

    Update code.cpp

--- InsertionSort/code.cpp
+++ InsertionSort/code.cpp
@@ -1,4 +1,5 @@
 //Insertion sort//
+// Role of this Sorting Algorithm -> Values from the unsorted part are picked and placed at the correct position in the sorted part.//
 
 #include<iostream>
 using namespace std;
@@ -6,9 +7,14 @@ using namespace std;
 int main()
 {
     int n;
-    cin>>n;
+    
+    //Takes the User Input
+    cin>>n;   
 
-    int arr[n];
+    //Here defining an array of size n//
+    int arr[n];   
+    
+    //Loop for taking the array elements
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
@@ -16,8 +22,10 @@ int main()
 
 
 //Algorithm//
+    // The counting starts from the 2nd element in the array list.
     for(int i=1;i<n;i++)
     {
+        //Now,comparing the current element (key) to its predecessor elements and moving the greater elements one position forward.
         int current =arr[i];
         int j=i-1;
         while(arr[j]>current && j>=0)
@@ -28,7 +36,7 @@ int main()
     arr[j+1]=current;
     }
 
-    //priting
+    //priting 
     for(int i=0;i<n;i++)
     {
         cout<<arr[i]<<" ";
